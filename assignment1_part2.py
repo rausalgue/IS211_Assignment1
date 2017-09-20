ddb2d9e3f022e80bb982e40b19347843f0264faf
Rommel Lasso <soccerply@msn.com>
2017-09-20 11:31:13 -0400

    Final Code
    Signed-off-by: Rommel Lasso <soccerply@msn.com>

--- assignment1_part2.py
+++ assignment1_part2.py
@@ -3,6 +3,20 @@
 """Assignemnt Week One - Part 2"""
 
 class Book():
-    print "hello"
+    type = "paperback"
 
-print "New Commit Value"
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    def display(self,):
+        print(self.title + " written by " + self.author)
+
+
+firstBook = Book("Of Mice and Men","John Steinbeck")
+
+secondBook = Book("To Kill a Mockingbird","Harper Lee")
+
+firstBook.display()
+
+secondBook.display()
